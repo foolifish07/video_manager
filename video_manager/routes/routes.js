98fedc109f5faaa4fbda5936cf50ade562c71951
foolifish07 <foolifish07@gmail.com>
2016-05-25 19:10:48 +0800

    adasdf

--- video_manager/routes/routes.js
+++ video_manager/routes/routes.js
@@ -6,6 +6,7 @@ var session = require('express-session');
 // tools
 var path = require('path');
 
+
 // sub route
 router.use('/api/user', require('./user'));
 router.use('/api/video', require('./video'));
@@ -14,9 +15,15 @@ router.use('/api/tags', require('./tags'));
 router.use('/api/tag', require('./tag'));
 router.use('/api/videotag', require('./videotag'));
 
-var index = path.join(__dirname, '..', 'public', 'pages', 'test.html');
-router.get('/', function(req, res, next){
-	res.sendFile(index);
+var test_path = path.join(__dirname, '..', 'public', 'pages', 'test.html');
+router.get('/test', function(req, res, next){
+	res.sendFile(test_path);
 })
+
+var index_path = path.join(__dirname, '..', 'public', 'pages', 'index.html');
+router.get('/', function(req, res, next){
+	res.sendFile(index_path);
+})
+
 
 module.exports = router;
